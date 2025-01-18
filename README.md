5f2d7a8eb6d527d6e4da9fb3c20fa1161b9e4681
Anderson Gonzalez <andersongonzalez2000@gmail.com>
2025-01-18 17:25:35 -0500

    Desafio completo

---
+++ README.md
@@ -0,0 +1,3 @@
+```Challenge ONE Lógica de Programación - Amigo secreto | Trello```
+
+En este desafío, desarrollarás una aplicación que permita a los usuarios ingresar nombres de amigos en una lista para luego realizar un sorteo aleatorio y determinar quién es el "amigo secreto".
--- app.js
+++ app.js
@@ -1,1 +1,36 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+const amigos = [];
+
+function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const nombre = input.value.trim(); 
+
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+    amigos.push(nombre);
+
+    const lista = document.getElementById("listaAmigos");
+    const listItem = document.createElement("li");
+    listItem.textContent = nombre;
+    lista.appendChild(listItem);
+
+    input.value = "";
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay amigos en la lista para sortear.");
+        return;
+    }
+
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+
+    const amigoSorteado = amigos[indiceAleatorio];
+
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li>El amigo sorteado es: <strong>${amigoSorteado}</strong></li>`;
+}
